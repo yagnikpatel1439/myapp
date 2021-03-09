fa8cce10e3a2c2fa7af41a39741ac6b2600f0a65
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2021-03-08 20:08:40 -0500

    layouting and structuring skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,65 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-item-group
+          v-model="selectedItem"
+          color="primary"
+        >
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+          >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dense>
+      <v-icon left  @click.native="sideNav = !sideNav">
+        fas fa-bars 
+      </v-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
 
       <v-spacer></v-spacer>
+      
+      <v-toolbar-items>
+        <v-btn elevation="2"> 
+          <v-icon left small>
+            fas fa-users
+          </v-icon>
+          Meetups
+        </v-btn>
+      </v-toolbar-items>
+      
+    </v-toolbar>
+    <main dark>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    </main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+   
   },
 
   data: () => ({
-    //
+      sideNav: false
   }),
 };
 </script>
+<style scoped>
+
+</style>
