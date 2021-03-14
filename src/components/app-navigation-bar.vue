3296d6617ef5865374f966443b22c237559d41eb
Venus Vavadiya <thisisvenusvavadiya@gmail.com>
2021-03-13 20:37:36 -0600

    AppBar done

---
+++ src/components/app-navigation-bar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar app>
+    <v-app-bar-nav-icon left  @click="$emit('click:side-nav')" class="hidden-sm-and-up"> </v-app-bar-nav-icon>
+
+    <v-toolbar-title  class="font-weight-bold">
+      <router-link to="/" tag="span" style="cursor: pointer">
+        DevMeetup
+      </router-link>
+    </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-xs-only" v-for="item in menuItems" :key="item.title">
+      <v-btn elevation="3" :to="item.link">
+        <v-icon left small>
+          {{item.icon}}
+        </v-icon>
+        {{item.title}}
+      </v-btn>
+    </v-toolbar-items>
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  props: {
+    menuItems: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      sideNav: false,
+    };
+  },
+};
+</script>
---
+++ src/components/app-navigation-drawer.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-navigation-drawer
+    absolute
+    app
+    temporary
+    :value="sideNav"
+    @input="handleInput"
+  >
+    <v-list
+      dense
+      nav
+    >
+      <v-list-item
+        v-for="item in menuItems"
+        :key="item.title"
+        link
+        :to="item.link"
+      >
+        <v-icon
+          v-if="item.icon"
+          left
+          small
+        >
+          {{ item.icon }}
+        </v-icon>
+
+        <v-list-item-title>
+          {{ item.title }}
+        </v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    menuItems: {
+      type: Array,
+      required: true,
+    },
+
+    sideNav: Boolean,
+  },
+
+  methods: {
+    handleInput(value) {
+      if (!value) this.$emit('close');
+    },
+  },
+};
+</script>
