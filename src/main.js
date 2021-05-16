c96616e73814bef3b23d1a5011d8dfc56ae8b13e
yagnikpatel1439 <yagnikpatel1439@gmail.com>
2021-05-15 22:13:28 -0400

    firebase added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
+import * as firebase from 'firebase'
 import {store} from './store'
 import DateFilter from './filters/date'
 import Vuetify from 'vuetify/lib/framework'
@@ -15,5 +16,14 @@ new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: "AIzaSyB0_eWgQxyQRZ0bsQ6NghDEFDQfX_yQ0tY",
+      authDomain: "meetup-vue-d3599.firebaseapp.com",
+      databaseUrl: "https://meetup-vue-d3599.firebaseapp.com",
+      projectId: "meetup-vue-d3599",
+      storageBucket: "meetup-vue-d3599.appspot.com",
+    })
+  }
 }).$mount('#app')
